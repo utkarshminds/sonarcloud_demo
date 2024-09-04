87fe817b94b5325590e842d3ee0a28493a9a1c4b
utkarshminds <pranav.nerurkar@utkarshminds.com>
2024-09-04 17:05:01 +0530

    submit the helloworld file

--- src/helloworld.py
+++ src/helloworld.py
@@ -1,1 +1,21 @@
 print('hello world')
+
+foo = 3+4
+
+def dummyfunction():
+    """
+    dummyfunction is empty for testing
+    """
+    pass
+
+
+basic = 3/0
+
+if None:
+    print('i am here in if')
+else:
+    print('i am in else')
+
+
+
+
